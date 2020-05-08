8370d0c05ef6441d68d466be816287ad8f782e07
TusharJoy <desmond.kuet@gmail.com>
2020-05-08 15:40:37 +0600

    First Commit

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>This is Single User with ID: {{$route.params.id}}</h1>
+</template>
+
+<script>
+  export default {
+      validate ({ params }) {
+          // Must be a number
+          console.log(params);
+          return /^\d+$/.test(params.id)
+      }
+  }
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,4 @@
+<template>
+<p>Just users directory</p>
+</template>
+
